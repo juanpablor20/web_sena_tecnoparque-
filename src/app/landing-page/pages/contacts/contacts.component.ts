4872e55f5a7e54b531558b9aae6749a873ee8491
juan pablo JPR <128744584+juanpablor20@users.noreply.github.com>
2024-11-18 15:04:42 -0500

    cambios en el navbar

--- src/app/landing-page/pages/contacts/contacts.component.ts
+++ src/app/landing-page/pages/contacts/contacts.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
