1d347b59476a9c2f6160801a07345f517be9a9ba
juan pablo JPR <128744584+juanpablor20@users.noreply.github.com>
2024-12-18 00:02:46 -0500

    control de cambios en  fincas

--- src/app/core/services/farms.service.ts
+++ src/app/core/services/farms.service.ts
@@ -10,27 +10,27 @@ export class FarmsService {
     {
       id: 1,
       name: 'Mirador Monte Café',
-      image: '/assets/img/finca_cafe/Finca1Cafe.jpg',
+      image: '/assets/img/images.jpg',
       description: 'Tipo de Productor: cafetero',
       link: '/estate/1',
-      backGroundimage: '/assets/img/finca_cafe/Finca1Cafe.jpg',
+      backGroundimage: '/assets/img/images.jpg',
       imagen2: '/assets/img/finca_cafe/Finca1Cafe.jpg',
       imagen3: '/assets/img/finca_cafe/Finca1Cafe.jpg'
     },
     {
       id: 2,
       name: 'La Argentina',
-      image: '/assets/img/finca_cafe/Finca2.jpg',
+      image: '/assets/img/cafe1.jpg',
       description: 'Tipo de Productor: cafetero.',
       link: '/estate/1',
-      backGroundimage: '/assets/img/finca_cafe/Finca2.jpg',
+      backGroundimage: '/assets/img/cafe1.jpg',
       imagen2: '/assets/img/finca_cafe/Finca2.jpg',
       imagen3: '/assets/img/finca_cafe/Finca2.jpg'
     },
     {
       id: 3,
       name: 'Buena vista',
-      image: '/assets/img/finca_cafe/Finca3.jpg',
+      image: '/assets/img/cafe3.jpg',
       description: 'Tipo de Productor: cafetero',
       link: '/estate/1',
       backGroundimage: '/assets/img/finca_cafe/Finca3.jpg',
@@ -40,7 +40,7 @@ export class FarmsService {
     {
       id: 4,
       name: ' La Vega',
-      image: '/assets/img/finca_cacao/Finca1.jpg',
+      image: '/assets/img/cacao1.jpg',
       description: 'Tipo de Productor: cacaotero.',
       prueba: ' por franklin cuellar ',
       link: '/estate/2',
@@ -48,14 +48,14 @@ export class FarmsService {
     {
       id: 5,
       name: 'Centro de Formación Agroindustrial La Angostura',
-      image: '/assets/img/finca_cacao/Finca2.jpg',
+      image: '/assets/img/cacao2.jpg',
       description: 'Tipo de Productor: cacaotero.',
       link: '/estate/3',
     },
     {
       id: 6,
       name: 'San Rafael',
-      image: '/assets/img/finca_cacao/Finca3Cacao.jpg',
+      image: '/assets/img/cacao3.jpg',
       description: 'Tipo de Productor: cacaotero.',
       link: '/estate/4',
     },
