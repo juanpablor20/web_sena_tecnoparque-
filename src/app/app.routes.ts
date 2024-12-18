6b17e038962ebb2b566599bc55ef44795940feff
juan pablo JPR <128744584+juanpablor20@users.noreply.github.com>
2024-12-18 12:58:42 -0500

    control de cambios en el sistema

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { Routes } from '@angular/router';
 import { ViewsEstateComponent } from './landing-page/pages/views-estate/views-estate.component';
 import { LoginComponent } from './auth/login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import DashboardComponent from './dashboard/dashboard.component';
 import { CacaoMetodologyComponent } from './landing-page/pages/cacao-metodology/cacao-metodology.component';
 import { CoffeMetodologyComponent } from './landing-page/pages/coffe-metodology/coffe-metodology.component';
 import { ContactsComponent } from './landing-page/pages/contacts/contacts.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { AuthGuard } from './core/guards/auth.guard';
+
 
 export const routes: Routes = [
 
@@ -17,7 +17,7 @@ export const routes: Routes = [
 
   {path: 'home', component: LandingPageComponent},
   { path: 'hola', component: ViewsEstateComponent },
-  { path: 'finca/:id', component: ViewsEstateComponent },
+  { path: 'estate/:id', component: ViewsEstateComponent },
   {path: 'login', component: LoginComponent },
   {path: 'MetodologiaCacao', component: CacaoMetodologyComponent},
  {path: 'contacts', component: ContactsComponent},
